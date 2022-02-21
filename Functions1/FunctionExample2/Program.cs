d323850a3b38f9f3fdb9d2717dcd5ae1ef2d746e
Erdem Kapkara <erdemkapkara.uk@gmail.com>
2022-02-21 16:10:31 +0000

    FirstTemplate

---
+++ Functions1/FunctionExample2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FunctionExample2
+{
+    class Program
+    {
+        static int function(int k, int costs_count, int[] costs)
+        {
+            int canAchieve = 0;
+            int temp = 0;
+            int repeatControl = 0;
+
+            for (int i=0; i < costs_count; i++)
+                {
+                    for(int j = i-1; j >= 0; j--)
+                    {
+                        temp += costs[j];
+                    
+                        if (temp > k && repeatControl<1)
+                            {
+                                i++;
+                                repeatControl++;
+                            }
+                    }
+                if (temp <= k)
+                {
+                    canAchieve += 1;
+                }
+            }
+            return canAchieve;
+        }
+        public static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 6, 7, 3, 4, 8 };
+            int count = 7;
+            int wallet = 14;
+            Console.WriteLine(Program.function(wallet, count, array));
+        }
+    }
+}
